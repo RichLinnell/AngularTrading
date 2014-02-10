b5f7a43e25755485547d55bc95a5e91adf8b3a53
Rich Linnell <richlinnell@gmail.com>
2014-02-10 14:57:36 +0000

    Include Bootstrap and Angular

--- AngularTrading/AngularTrading/App_Start/BundleConfig.cs
+++ AngularTrading/AngularTrading/App_Start/BundleConfig.cs
@@ -17,7 +17,13 @@ namespace AngularTrading
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.min.js",
+                "~/Scripts/angular-touch.min.js",
+                "~/Scripts/ui-bootstrap-0.10.0.min.js")
+                );
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.min.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -37,7 +43,9 @@ namespace AngularTrading
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.theme.css",
+                        "~/Content/bootstrap.min.css",
+                        "~/Content/bootstrap-theme.min.css"));
         }
     }
 }
